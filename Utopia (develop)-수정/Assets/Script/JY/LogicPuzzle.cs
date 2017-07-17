60f25efd0666b46ab4fe1ddd3ab5b6307947c847
김준연 <mynamejohn@naver.com>
2017-07-17 13:27:53 +0900

    7월 17일 시작

--- Utopia (develop)-수정/Assets/Script/JY/LogicPuzzle.cs
+++ Utopia (develop)-수정/Assets/Script/JY/LogicPuzzle.cs
@@ -2,28 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LogicPuzzle : MonoBehaviour {
+public class LogicPuzzle : MonoBehaviour
+{
 
     GameObject[] Tile_Obj = new GameObject[100];
     public Tile[] t = new Tile[100];
 
     public GameObject LogicPuzControl;
     public bool Activated = false;
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         LogicPuzControl.SetActive(true);
-        for (int i=0;i<100;i++)
+        for (int i = 0; i < 100; i++)
         {
-            Tile_Obj[i] = GameObject.Find("Tile (" + (i+1).ToString() + ")");
+            Tile_Obj[i] = GameObject.Find("Tile (" + (i + 1).ToString() + ")");
             t[i] = Tile_Obj[i].GetComponent<Tile>();
         }
         LogicPuzControl.SetActive(false);
 
+        t[12].is_Cliked = true;
+        t[13].is_Cliked = true;
+        t[14].is_Cliked = true;
+        t[15].is_Cliked = true;
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         if (!Activated)
         {
@@ -32,16 +37,17 @@ public class LogicPuzzle : MonoBehaviour {
         }
         LogicPuzControl.SetActive(true);
 
-        if (t[0].is_Cliked && t[1].is_Cliked && t[2].is_Cliked && t[3].is_Cliked)
+        if (t[2].is_Cliked && t[3].is_Cliked && t[4].is_Cliked && t[5].is_Cliked && t[6].is_Cliked && t[7].is_Cliked && t[11].is_Cliked && t[12].is_Cliked && t[13].is_Cliked && t[14].is_Cliked && t[15].is_Cliked && t[16].is_Cliked && t[17].is_Cliked && t[18].is_Cliked
+            && t[18].is_Cliked)
         {
             Debug.Log("정답");
-            Activated = false ;
+            Activated = false;
         }
-	}
+    }
 
     public void Clear_Tile()
     {
-        for(int i=0;i<100;i++)
+        for (int i = 0; i < 100; i++)
         {
             t[i].is_Cliked = false;
         }
