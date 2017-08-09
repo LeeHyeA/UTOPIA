c79188531f7571975daf0506ff3f01787d8832c4
김준연 <mynamejohn@naver.com>
2017-08-09 15:30:18 +0900

    no message

--- Utopia (develop)-수정/Assets/Script/JY/SlidePuzzle.cs
+++ Utopia (develop)-수정/Assets/Script/JY/SlidePuzzle.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SlidePuzzle : MonoBehaviour {
-
+    
     int width = 191;
     int height = 206;
 
     public SlidePuzzleTile[,] Tiles = new SlidePuzzleTile[4,3];
+    SlidePuzzleTile blank_Tile;
 
 	// Use this for initialization
 	void Start ()
@@ -22,9 +23,9 @@ public class SlidePuzzle : MonoBehaviour {
                 Tiles[i, j].Ver = j;
             }
         }
+        blank_Tile = Tiles[3, 2];
+        blank_Tile.is_blank = true;
 
-        Tiles[3, 2].is_blank = true;
-        
         /*for (int i = 0; i < 4; i++)
         {
             for (int j = 0; j < 3; j++)
@@ -40,4 +41,22 @@ public class SlidePuzzle : MonoBehaviour {
     {
 		
 	}
+
+    public void CheckDirection(SlidePuzzleTile SPT)
+    {
+        if (SPT.Ver == blank_Tile.Ver && SPT.Hor == blank_Tile.Hor)
+            return;
+
+        if (blank_Tile.Ver == SPT.Ver)
+        {
+            if (blank_Tile.Hor - SPT.Hor == 1)
+            {
+                SPT.triggerMoving(0);
+            }
+            if (SPT.Hor - blank_Tile.Hor == 1)
+            {
+                SPT.triggerMoving(2);
+            }
+        }
+    }
 }
--- Utopia (develop)-수정/Assets/Script/JY/SlidePuzzleTile.cs
+++ Utopia (develop)-수정/Assets/Script/JY/SlidePuzzleTile.cs
@@ -4,20 +4,83 @@ using UnityEngine;
 
 public class SlidePuzzleTile : MonoBehaviour {
 
+    SlidePuzzle SP;
     Transform Tf;
     public int Hor;    //가로
     public int Ver;    //세로
 
+    int direction; // 왼 - 위 - 오 - 아 : 0 - 1 - 2 - 3
     public bool is_blank = false;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-        Tf = GetComponent<Transform>();	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        SP = FindObjectOfType<SlidePuzzle>();
+        Tf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnMouseDown()
+    {
+        SP.CheckDirection(this);
+    }
+
+    public void triggerMoving(int dir)
+    {
+        direction = dir;
+        StartCoroutine(moving(direction));
+    }
+    IEnumerator moving(int direction)
+    {
+        int i = 0;
+        switch(direction)
+        {
+            case 0:
+                for (i = 0; i < 190; i++)
+                {
+                    Vector3 Vec = gameObject.transform.position;
+                    Vec.x = Vec.x -1;
+                    gameObject.transform.position = Vec;
+
+                    yield return new WaitForSeconds(0.1f);
+                }
+                break;
+            case 1:
+                for (i = 0; i < 206; i++)
+                {
+                    Vector3 Vec = gameObject.transform.position;
+                    Vec.y = Vec.y + 1;
+                    gameObject.transform.position = Vec;
+
+                    yield return new WaitForSeconds(0.1f);
+                }
+                break;
+            case 2:
+                for (i = 0; i < 190; i++)
+                {
+                    Vector3 Vec = gameObject.transform.position;
+                    Vec.x = Vec.x + 1;
+                    gameObject.transform.position = Vec;
+
+                    yield return new WaitForSeconds(0.1f);
+                }
+                break;
+            case 3:
+                for (i = 0; i < 206; i++)
+                {
+                    Vector3 Vec = gameObject.transform.position;
+                    Vec.y = Vec.y - 1;
+                    gameObject.transform.position = Vec;
+
+                    yield return new WaitForSeconds(0.1f);
+                }
+                break;
+        }
+
+        yield break;
+    }
 }
