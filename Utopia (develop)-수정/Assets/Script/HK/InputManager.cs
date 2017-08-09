2e9d78c46a74666360f24e1e74a118107cdb340c
LeeHyeA <951220_@naver.com>
2017-08-09 15:31:17 +0900

    수정!

--- Utopia (develop)-수정/Assets/Script/HK/InputManager.cs
+++ Utopia (develop)-수정/Assets/Script/HK/InputManager.cs
@@ -644,6 +644,7 @@ public class InputManager : MonoBehaviour
             if (obj.transform.name == "6-FlowerRing" && hit.transform.name == "Statuette")
             {
                 FlowerRing = true;
+				Stage3.Find ("1Round").Find ("FlowerRing").gameObject.SetActive(true);
                 Destroy(obj.transform.gameObject);
                 Check();
             }
@@ -651,6 +652,7 @@ public class InputManager : MonoBehaviour
             if (obj.transform.name == "4-Shell" && hit.transform.name == "Statuette")
             {
                 Shell = true;
+				Stage3.Find ("1Round").Find ("Shell").gameObject.SetActive(true);
                 Destroy(obj.transform.gameObject);
                 Check();
             }
